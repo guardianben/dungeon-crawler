a447c9fb6540410500f02cd81845b0437b7ee7ba
guardianben <guardian.nova@gmail.com>
2017-04-20 13:46:50 -0400

    Add files via upload

---
+++ javascript/objectPrototypes.js
@@ -0,0 +1,16 @@
+function Self(name,race,health,magic,xp,str,agi,con,int,wis){
+	this.Name = name;
+	this.Race = race;
+	this.Health = health;
+	this.Magic = magic;
+	this.XP = xp;
+	this.Strength = str;
+	this.Agility = agi;
+	this.Constitution = con;
+	this.Intelligence = int;
+	this.Wisdom = wis;
+}
+
+function Enemy(){
+	
+}
