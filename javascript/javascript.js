901897069a25c385014a5908b2aa9026949a52df
guardianben <guardian.nova@gmail.com>
2017-04-21 16:24:32 -0400

    Add files via upload

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -27,8 +27,10 @@ function removePoint(stat,obj){
 function confirmChar(){
 	if(document.getElementsByName("TxName")[0].value != ""){
 		if(confirm("Confirm your character.\n\nName: " + document.getElementsByName("TxName")[0].value + "\n\nStrength: " + player.Strength + "\nAgility: " + player.Agility + "\nConstitution: " + player.Constitution + "\nIntelligence: " + player.Intelligence + "\nWisdom: " + player.Wisdom) == true){
+			player.Name = document.getElementsByName("TxName")[0].value;
 			player.Health = player.Constitution * 5;
 			player.Magic = player.Intelligence * 5;
+			loadChar();
 			toggle('gameScreen','newGame');
 		}
 	}
@@ -40,7 +42,90 @@ function confirmChar(){
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+function loadChar(){
+	$("#selfName").empty();
+	$("#selfName").append(player.Name);
+	$("#selfHealth").empty();
+	$("#selfHealth").append(player.Health);
+	$("#selfMagic").empty();
+	$("#selfMagic").append(player.Magic);
+	$("#selfXp").empty();
+	$("#selfXp").append(player.XP);
+	$("#selfStr").empty();
+	$("#selfStr").append(player.Strength);
+	$("#selfAgi").empty();
+	$("#selfAgi").append(player.Agility);
+	$("#selfCon").empty();
+	$("#selfCon").append(player.Constitution);
+	$("#selfInt").empty();
+	$("#selfInt").append(player.Intelligence);
+	$("#selfWis").empty();
+	$("#selfWis").append(player.Wisdom);
+	$("#selfGold").empty();
+	$("#selfGold").append(player.Gold);
+	$("#selfHead").empty();
+	if(equip.Head == false){
+		$("#selfHead").append(nothing.Name);
+	}
+	else{
+		$("#selfHead").append(equip.Head.Name);
+	}
+	$("#selfShoulder").empty();
+	if(equip.Shoulders == false){
+		$("#selfShoulder").append(nothing.Name);
+	}
+	else{
+		$("#selfShoulder").append(equip.Shoulders.Name);
+	}
+	$("#selfChest").empty();
+	if(equip.Chest == false){
+		$("#selfChest").append(shirt.Name);
+	}
+	else{
+		$("#selfChest").append(equip.Chest.Name);
+	}
+	$("#selfArms").empty();
+	if(equip.Arms == false){
+		$("#selfArms").append(nothing.Name);
+	}
+	else{
+		$("#selfArms").append(equip.Arms.Name);
+	}
+	$("#selfLegs").empty();
+	if(equip.Legs == false){
+		$("#selfLegs").append(pants.Name);
+	}
+	else{
+		$("#selfLegs").append(equip.Legs.Name);
+	}
+	$("#selfFeet").empty();
+	if(equip.Feet == false){
+		$("#selfFeet").append(nothing.Name);
+	}
+	else{
+		$("#selfFeet").append(equip.Feet.Name);
+	}
+	$("#selfMainHand").empty();
+	if(equip.Main_Hand == false){
+		$("#selfMainHand").append(fists.Name);
+	}
+	else{
+		$("#selfMainHand").append(equip.Main_Hand.Name);
+	}
+	$("#selfOffHand").append();
+	if(equip.Off_Hand == false){
+		$("#selfOffHand").append(fists.Name);
+	}
+	else{
+		$("#selfOffHand").append(equip.Off_hand.Name);
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 function toggle(obj1,obj2){
 	$("#" + obj2).hide();
 	$("#" + obj1).show();
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
--- javascript/objectPrototypes.js
+++ javascript/objectPrototypes.js
@@ -1,4 +1,4 @@
-function Self(name,level,health,magic,xp,str,agi,con,Int,wis,rempoint,inv,loc,gold){
+function Self(name,level,health,magic,xp,str,agi,con,Int,wis,rempoint,loc,gold){
 	this.Name = name;
 	this.Level = level;
 	this.Health = health;
@@ -10,9 +10,19 @@ function Self(name,level,health,magic,xp,str,agi,con,Int,wis,rempoint,inv,loc,go
 	this.Intelligence = Int;
 	this.Wisdom = wis;
 	this.pointsRemain = rempoint;
-	this.Inventory = inv;
 	this.Location = loc;
 	this.Gold = gold;
+}
+
+function Equipment(head,shoulders,chest,arms,legs,feet,mHand,oHand){
+	this.Head = head;
+	this.Shoulders = shoulders;
+	this.Chest = chest;
+	this.Arms = arms;
+	this.Legs = legs;
+	this.Feet = feet;
+	this.Main_Hand = mHand;
+	this.Off_Hand = oHand;
 }
 
 function Enemy(type,health,magic,challenge,defense,attack,loc){
@@ -26,9 +36,14 @@ function Enemy(type,health,magic,challenge,defense,attack,loc){
 }
 
 function Gear(type,name,stats,level,rarity){
-	this.Type = type; //type - equip location: head,shoulders,chest,feet,arms,main hand,off hand
+	this.Type = type; //type - equip location: head,shoulders,chest,legs,feet,arms,main hand,off hand,any hand,
 	this.Name = name;
 	this.Stats = stats; //stats - array [min-damage,max-damage,crit-chance,crit-multiplier,accuracy,defense]
 	this.Level = level;
 	this.Rarity = rarity;
 }
+
+var shirt = new Gear('chest','shirt',[false,false,false,false,false,0],1,0);
+var pants = new Gear('legs','pants',[false,false,false,false,false,0],1,0);
+var fists = new Gear('any hand','fists',[0,0,5,2,70,false],1,0);
+var nothing = new Gear('special','nothing',[false,false,false,false,false,false],1,0);
