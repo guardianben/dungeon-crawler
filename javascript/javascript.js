cd5a679c4d39392bb33e5bf7ed56f539fd7fff32
guardianben <guardian.nova@gmail.com>
2017-04-21 14:21:08 -0400

    Add files via upload

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,1 +1,46 @@
+function addPoint(stat,obj){
+	if(player.pointsRemain > 0){
+		player[stat]++;
+		$("#" + obj).empty();
+		$("#" + obj).append(player[stat]);
+		player.pointsRemain--;
+		$("#PointsRemain").empty();
+		$("#PointsRemain").append(player.pointsRemain);
+	}
+}
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function removePoint(stat,obj){
+	if(player[stat] > 1){
+		player[stat]--;
+		$("#" + obj).empty();
+		$("#" + obj).append(player[stat]);
+		player.pointsRemain++;
+		$("#PointsRemain").empty();
+		$("#PointsRemain").append(player.pointsRemain);
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function confirmChar(){
+	if(document.getElementsByName("TxName")[0].value != ""){
+		if(confirm("Confirm your character.\n\nName: " + document.getElementsByName("TxName")[0].value + "\n\nStrength: " + player.Strength + "\nAgility: " + player.Agility + "\nConstitution: " + player.Constitution + "\nIntelligence: " + player.Intelligence + "\nWisdom: " + player.Wisdom) == true){
+			player.Health = player.Constitution * 5;
+			player.Magic = player.Intelligence * 5;
+			toggle('gameScreen','newGame');
+		}
+	}
+	else{
+		document.getElementsByName("TxName")[0].style.background="#f00";
+		alert("Please provide a name for your character.");
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function toggle(obj1,obj2){
+	$("#" + obj2).hide();
+	$("#" + obj1).show();
+}
--- javascript/objectPrototypes.js
+++ javascript/objectPrototypes.js
@@ -1,5 +1,6 @@
-function Self(name,health,magic,xp,str,agi,con,Int,wis,rempoint){
+function Self(name,level,health,magic,xp,str,agi,con,Int,wis,rempoint,inv,loc,gold){
 	this.Name = name;
+	this.Level = level;
 	this.Health = health;
 	this.Magic = magic;
 	this.XP = xp;
@@ -9,21 +10,25 @@ function Self(name,health,magic,xp,str,agi,con,Int,wis,rempoint){
 	this.Intelligence = Int;
 	this.Wisdom = wis;
 	this.pointsRemain = rempoint;
+	this.Inventory = inv;
+	this.Location = loc;
+	this.Gold = gold;
 }
 
-function Enemy(type,health,magic,challenge,defense,attack){
+function Enemy(type,health,magic,challenge,defense,attack,loc){
 	this.Type = type;
 	this.Health = health;
 	this.Magic = magic;
 	this.Challenge = challenge;
 	this.Defense = defense;
 	this.Attack = attack;
+	this.Location = loc;
 }
 
-function Gear(type,name,stats,level,rarity){//stats - array [min-damage,max-damage,crit-chance,crit-multiplier,accuracy,defense] || type - equip location: head,shoulders,chest,feet,arms,main hand,off hand
-	this.type = type;
-	this.name = name;
-	this.stats = stats;
-	this.level = level;
-	this.rarity = rarity;
+function Gear(type,name,stats,level,rarity){
+	this.Type = type; //type - equip location: head,shoulders,chest,feet,arms,main hand,off hand
+	this.Name = name;
+	this.Stats = stats; //stats - array [min-damage,max-damage,crit-chance,crit-multiplier,accuracy,defense]
+	this.Level = level;
+	this.Rarity = rarity;
 }
